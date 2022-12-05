d7fcad2fbe32841356402c3591843615c7edf7be
gnoeley <leonpelech@gmail.com>
2022-12-05 21:35:20 +0000

    Refactor Day02

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,3 +1,5 @@
+import Day02.Companion.Shape.Companion.fromStrategy
+
 fun main() {
     val input = "input/day02-a.txt".readLines()
     println("Day 02 - A result = ${Day02.solveA(input)}")
@@ -7,60 +9,49 @@ fun main() {
 class Day02 {
     companion object {
 
-        private const val P_ROCK = "X"
-        private const val P_PAPER = "Y"
-        private const val P_SCISSOR = "Z"
+        private enum class Shape(val score: Int) {
+            ROCK(1),
+            PAPER(2),
+            SCISSORS(3);
 
-        private const val P_LOOSE = "X"
-        private const val P_DRAW = "Y"
-        private const val P_WIN = "Z"
+            companion object {
+                fun fromStrategy(input: String) = when(input) {
+                    "A", "X" -> ROCK
+                    "B", "Y" -> PAPER
+                    "C", "Z" -> SCISSORS
+                    else -> throw RuntimeException("Invalid shape: $input")
+                }
+            }
 
-        private const val O_ROCK = "A"
-        private const val O_PAPER = "B"
-        private const val O_SCISSOR = "C"
-
-        private val shapeScore = mapOf(
-            P_ROCK to 1,
-            P_PAPER to 2,
-            P_SCISSOR to 3,
-        )
-
-        private fun outcomeScore(opponent: String, player: String) = when {
-            opponent == O_ROCK && player == P_SCISSOR ||
-                    opponent == O_PAPER && player == P_ROCK ||
-                    opponent == O_SCISSOR && player == P_PAPER -> 0
-            opponent == O_ROCK && player == P_PAPER ||
-                    opponent == O_PAPER && player == P_SCISSOR ||
-                    opponent == O_SCISSOR && player == P_ROCK -> 6
-            else -> 3
-        }
-
-        private fun chooseShape(opponent: String, outcome: String) = when {
-            opponent == O_ROCK && outcome == P_LOOSE -> P_SCISSOR
-            opponent == O_ROCK && outcome == P_DRAW -> P_ROCK
-            opponent == O_ROCK && outcome == P_WIN -> P_PAPER
-
-            opponent == O_PAPER && outcome == P_LOOSE -> P_ROCK
-            opponent == O_PAPER && outcome == P_DRAW -> P_PAPER
-            opponent == O_PAPER && outcome == P_WIN -> P_SCISSOR
-
-            opponent == O_SCISSOR && outcome == P_LOOSE -> P_PAPER
-            opponent == O_SCISSOR && outcome == P_DRAW -> P_SCISSOR
-            opponent == O_SCISSOR && outcome == P_WIN -> P_ROCK
-
-            else -> null
+            fun toWin() = values()[(ordinal + 1) % 3]
+            fun toDraw() = this
+            fun toLoose() = values()[(ordinal + 2) % 3]
+            fun outcomeScore(other: Shape) = when(other) {
+                toWin() -> 0
+                toDraw() -> 3
+                else -> 6
+            }
         }
 
         fun solveA(input: List<String>) = input.sumOf {
-            it.split(" ").let { round ->
-                shapeScore[round[1]]!! + outcomeScore(round[0], round[1])
+            it.split(" ").let { strategy ->
+                val opponentShape = fromStrategy(strategy[0])
+                val playerShape = fromStrategy(strategy[1])
+                playerShape.score + playerShape.outcomeScore(opponentShape)
             }
         }
 
         fun solveB(input: List<String>) = input.sumOf {
-            it.split(" ").let { round ->
-                val shape = chooseShape(round[0], round[1])!!
-                shapeScore[shape]!! + outcomeScore(round[0], shape)
+            it.split(" ").let { strategy ->
+                val opponentShape = fromStrategy(strategy[0])
+                val playerShape = when (strategy[1]) {
+                    "X" -> opponentShape.toLoose()
+                    "Y" -> opponentShape.toDraw()
+                    "Z" -> opponentShape.toWin()
+                    else -> throw RuntimeException("Invalid outcome: ${strategy[1]}")
+                }
+
+                playerShape.score + playerShape.outcomeScore(opponentShape)
             }
         }
     }

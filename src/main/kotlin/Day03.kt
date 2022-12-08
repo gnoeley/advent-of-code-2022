25177ab84c6c0fb53da99b0712532b951c39829b
gnoeley <leonpelech@gmail.com>
2022-12-08 21:27:55 +0000

    Refactor Day03
    - Use chunked, array destructure and in operator to clean-up code
    - Replace flatmap/groupingBy/filter with nested iteration
    and sumOf array destructured parameter
    - Reorder private method

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -1,5 +1,3 @@
-import java.util.function.Function
-
 fun main() {
     val input = "input/day03-b.txt".readLines()
 //    println("Day 03 - A result = ${Day03.solveA(input)}")
@@ -9,23 +7,15 @@ fun main() {
 object Day03 {
 
     fun solveA(input: List<String>): Int = input
-        .map { it.subSequence(0, it.length / 2) to it.subSequence(it.length / 2, it.length) }
-        .map { rucksack -> rucksack.first.first { rucksack.second.contains(it) } }
+        .map { backpack -> backpack.chunked(backpack.length / 2) }
+        .map { (firstPocket, secondPocket) -> firstPocket.first { it in secondPocket } }
         .sumOf { it.toItemPriority }
+
+    fun solveB(input: List<String>): Int = input
+        .chunked(3)
+        .sumOf { (first, second, third) -> first.first { it in second && it in third }.toItemPriority }
 
     private val Char.toItemPriority
         get() =
             (if (isLowerCase()) this - 96 else this - 38).code
-
-    fun solveB(input: List<String>): Int = input.chunked(3)
-        .sumOf { backpacks ->
-            backpacks
-                .flatMap { it.toSet() }
-                .groupingBy { it }
-                .eachCount()
-                .filter { it.value == 3 }
-                .keys
-                .first()
-                .toItemPriority
-        }
 }

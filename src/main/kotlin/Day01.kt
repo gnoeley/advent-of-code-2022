b04c50e8ee7fcfc054195111e5e2054fe94220d7
gnoeley <leonpelech@gmail.com>
2022-12-08 21:17:32 +0000

    Refactor Day01 make an object

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,29 +1,26 @@
-import kotlin.io.path.toPath
-
 fun main() {
     val input = "input/day01-a.txt".readLines()
     println("Day 01 - A result = ${Day01.solveA(input)}")
     println("Day 01 - B result = ${Day01.solveB(input)}")
 }
 
-class Day01 {
-    companion object {
-        fun solveA(input: List<String>) = input
-            .joinToString(separator = "-")
-            .split("--")
-            .maxOfOrNull {
-                it.split("-").sumOf { num -> num.toInt() }
-            }
+object Day01 {
 
-        fun solveB(input: List<String>) = input
-            .joinToString(separator = "-")
-            .split("--")
-            .map {
-                it.split("-").sumOf { num -> num.toInt() }
-            }
-            .sortedDescending()
-            .take(3)
-            .sum()
+    fun solveA(input: List<String>) = input
+        .joinToString(separator = "-")
+        .split("--")
+        .maxOfOrNull {
+            it.split("-").sumOf { num -> num.toInt() }
+        }
 
-    }
+    fun solveB(input: List<String>) = input
+        .joinToString(separator = "-")
+        .split("--")
+        .map {
+            it.split("-").sumOf { num -> num.toInt() }
+        }
+        .sortedDescending()
+        .take(3)
+        .sum()
+
 }

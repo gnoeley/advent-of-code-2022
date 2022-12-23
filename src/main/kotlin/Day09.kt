88a6ae4b0c4eee0ca55ae2a71010834859a2b748
gnoeley <leonpelech@gmail.com>
2022-12-23 17:53:49 +0000

    Extract reusable functions Day09

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -8,37 +8,44 @@ fun main() {
 
 object Day09 {
     fun solveA(input: List<String>): Int {
+        val moves = parseMoves(input)
+
         var head = 0 to 0
         var tail = 0 to 0
         val visited = mutableSetOf(tail.copy())
 
-        val moves = input.flatMap {
-            it.split(" ")
-                .let { (direction, times) ->
-                    (1..times.toInt()).map {
-                        when(direction) {
-                            "R" -> 1 to 0
-                            "L" -> -1 to 0
-                            "U" -> 0 to 1
-                            "D" -> 0 to -1
-                            else -> throw RuntimeException("Invalid direction: $direction")
-                        }
-                    }
-                }
-        }
-
         for ((x, y) in moves) {
             head = head.first + x to head.second + y
-            val diffX = tail.first - head.first // if > abs(2); move 1 in that direction
-            val diffY = tail.second - head.second
-
-
-            if (abs(diffX) > 1 || abs(diffY) > 1) {
-                tail = tail.first + (if (diffX != 0) (diffX / abs(diffX) * -1) else 0) to tail.second + (if (diffY != 0) (diffY / abs(diffY) * -1) else 0)
-                visited.add(tail.copy())
-            }
+            tail = nextTail(head, tail)
+            visited.add(tail.copy())
         }
         return visited.size
     }
+
     fun solveB(input: List<String>): Int = 0
+
+    private fun nextTail(head: Pair<Int, Int>, tail: Pair<Int, Int>): Pair<Int, Int> {
+        val diffX = tail.first - head.first // if > abs(2); move 1 in that direction
+        val diffY = tail.second - head.second
+        return if (abs(diffX) > 1 || abs(diffY) > 1) {
+            tail.first + (if (diffX != 0) (diffX / abs(diffX) * -1) else 0) to tail.second + (if (diffY != 0) (diffY / abs(diffY) * -1) else 0)
+        } else {
+            tail
+        }
+    }
+
+    private fun parseMoves(input: List<String>) = input.flatMap {
+        it.split(" ")
+            .let { (direction, times) ->
+                (1..times.toInt()).map {
+                    when (direction) {
+                        "R" -> 1 to 0
+                        "L" -> -1 to 0
+                        "U" -> 0 to 1
+                        "D" -> 0 to -1
+                        else -> throw RuntimeException("Invalid direction: $direction")
+                    }
+                }
+            }
+    }
 }

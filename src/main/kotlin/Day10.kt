2fea6fe8b7e1b007e2c2c724d24c29bbc162f503
gnoeley <leonpelech@gmail.com>
2022-12-23 21:44:33 +0000

    Solve Day10 part B - never been so confused by modulo
    So, just couldn't wrap my head around when to take off the
    1 to go to base-zero (before or after modulo) coupled with
    headache around modulo 39 vs 40 🤦. Seriously...
    Today was a hack/debug fest.

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,18 +1,18 @@
 fun main() {
     val input = "input/day10.txt".readLines()
     println("Day 10 - A result = ${Day10.solveA(input)}")
-//    println("Day 10 - B result = ${Day10.solveB(input)}")
+    println("Day 10 - B result = ${Day10.solveB(input)}")
 }
 
 
 object Day10 {
     fun solveA(input: List<String>): Int = input
-        .fold(setOf(State(1, 1))) { acc, command ->
+        .fold(listOf(State(1, 1))) { acc, command ->
             val previousCycle = acc.last()
             val parsed = command.split(" ")
             acc + when (parsed.size) {
-                1 -> setOf(previousCycle.copy(cycle = previousCycle.cycle + 1))
-                else -> setOf(
+                1 -> listOf(previousCycle.copy(cycle = previousCycle.cycle + 1))
+                else -> listOf(
                     previousCycle.copy(cycle = previousCycle.cycle + 1),
                     previousCycle.copy(
                         registerX = previousCycle.registerX + parsed[1].toInt(),
@@ -23,6 +23,26 @@ object Day10 {
         }
         .filter { it.cycle in (20..220 step 40) }
         .sumOf { it.registerX * it.cycle }
+
+    fun solveB(input: List<String>): String = input
+        .fold(listOf(State(1, 1))) { acc, command ->
+            val previousCycle = acc.last()
+            val parsed = command.split(" ")
+            acc + when (parsed.size) {
+                1 -> listOf(previousCycle.copy(cycle = previousCycle.cycle + 1))
+                else -> listOf(
+                    previousCycle.copy(cycle = previousCycle.cycle + 1),
+                    previousCycle.copy(
+                        registerX = previousCycle.registerX + parsed[1].toInt(),
+                        cycle = previousCycle.cycle + 2
+                    )
+                )
+            }
+        }
+        .windowed(step = 40, size = 40)
+        .joinToString("\n", prefix = "\n") {
+            it.joinToString("") { state -> if ((state.cycle - 1)  % 40 in (state.registerX - 1..state.registerX + 1)) "#" else "." }
+        }
 
     data class State(val registerX: Int, val cycle: Int)
 }

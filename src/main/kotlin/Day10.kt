d7d2d9aedd9013dba1ea8bfb5d1f5d31f2601f10
gnoeley <leonpelech@gmail.com>
2022-12-23 21:46:27 +0000

    Refactor duplicate code Day10

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -6,43 +6,33 @@ fun main() {
 
 
 object Day10 {
-    fun solveA(input: List<String>): Int = input
-        .fold(listOf(State(1, 1))) { acc, command ->
-            val previousCycle = acc.last()
-            val parsed = command.split(" ")
-            acc + when (parsed.size) {
-                1 -> listOf(previousCycle.copy(cycle = previousCycle.cycle + 1))
-                else -> listOf(
-                    previousCycle.copy(cycle = previousCycle.cycle + 1),
-                    previousCycle.copy(
-                        registerX = previousCycle.registerX + parsed[1].toInt(),
-                        cycle = previousCycle.cycle + 2
-                    )
-                )
-            }
-        }
+    fun solveA(input: List<String>): Int = computeCycles(input)
         .filter { it.cycle in (20..220 step 40) }
         .sumOf { it.registerX * it.cycle }
 
-    fun solveB(input: List<String>): String = input
-        .fold(listOf(State(1, 1))) { acc, command ->
-            val previousCycle = acc.last()
-            val parsed = command.split(" ")
-            acc + when (parsed.size) {
-                1 -> listOf(previousCycle.copy(cycle = previousCycle.cycle + 1))
-                else -> listOf(
-                    previousCycle.copy(cycle = previousCycle.cycle + 1),
-                    previousCycle.copy(
-                        registerX = previousCycle.registerX + parsed[1].toInt(),
-                        cycle = previousCycle.cycle + 2
-                    )
-                )
-            }
-        }
+    fun solveB(input: List<String>): String = computeCycles(input)
         .windowed(step = 40, size = 40)
         .joinToString("\n", prefix = "\n") {
-            it.joinToString("") { state -> if ((state.cycle - 1)  % 40 in (state.registerX - 1..state.registerX + 1)) "#" else "." }
+            it.joinToString("") {
+                    state -> if ((state.cycle - 1)  % 40 in (state.registerX - 1..state.registerX + 1)) "#" else "."
+            }
         }
+
+    private fun computeCycles(input: List<String>) = input
+            .fold(listOf(State(1, 1))) { acc, command ->
+                val previousCycle = acc.last()
+                val parsed = command.split(" ")
+                acc + when (parsed.size) {
+                    1 -> listOf(previousCycle.copy(cycle = previousCycle.cycle + 1))
+                    else -> listOf(
+                        previousCycle.copy(cycle = previousCycle.cycle + 1),
+                        previousCycle.copy(
+                            registerX = previousCycle.registerX + parsed[1].toInt(),
+                            cycle = previousCycle.cycle + 2
+                        )
+                    )
+                }
+            }
 
     data class State(val registerX: Int, val cycle: Int)
 }

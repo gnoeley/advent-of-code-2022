1d93e4832dcd4c96ecd85d934f85d2380f940909
gnoeley <leonpelech@gmail.com>
2022-12-27 00:40:14 +0000

    Tried with mod in the update to shrink storage

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -1,22 +1,44 @@
-import kotlin.math.floor
-
 fun main() {
     val input = "input/day11.txt".readLines()
-    println("Day 11 - A result = ${Day11.solveA(input)}")
-//    println("Day 11 - B result = ${Day11.solveB(input)}")
+//    println("Day 11 - A result = ${Day11.solveA(input)}")
+    println("Day 11 - B result = ${Day11.solveB(input)}")
 }
 
 
 object Day11 {
-    fun solveA(input: List<String>): Int = input
+    fun solveA(input: List<String>): UInt = input
         .joinToString("\n")
         .split("\n\n")
-        .map { Monkey.from(it) }
+        .map { Monkey.from(it, true) }
         .let { monkeys ->
             monkeys.forEachIndexed { index, monkey ->
                 monkeys.forEach { other -> other.monkeyDiscovered(index, monkey) }
             }
-            (1 .. 20).forEach { monkeys.forEach { it.doRound() } }
+            repeat((1..20).count()) {
+                monkeys.forEach { it.doRound() }
+            }
+            monkeys
+        }
+        .sortedByDescending { it.inspections }
+        .take(2)
+        .let { (first, second) -> first.inspections * second.inspections }
+
+    fun solveB(input: List<String>): UInt = input
+        .joinToString("\n")
+        .split("\n\n")
+        .map { Monkey.from(it, false) }
+        .let { monkeys ->
+            monkeys.forEachIndexed { index, monkey ->
+                monkeys.forEach { other -> other.monkeyDiscovered(index, monkey) }
+            }
+            var iterations = 0
+            repeat((1..10000).count()) {
+                iterations++
+                monkeys.forEach {
+                    it.doRound()
+                }
+                if (iterations % 100 == 0) println("Total iterations: $iterations")
+            }
             monkeys
         }
         .sortedByDescending { it.inspections }
@@ -25,59 +47,55 @@ object Day11 {
 }
 
 class Monkey private constructor(
-    val items: MutableList<Item>,
-    private val inspectFun: (worry: Int) -> Int,
-    private val checkCondition: (worry: Int) -> Boolean,
+    private val items: MutableList<Item>,
+    private val inspectFun: (worry: Long) -> Long,
     private val throwOnTrue: Int,
     private val throwOnFalse: Int,
-    var inspections: Int = 0,
-    val otherMonkeys: MutableMap<Int, Monkey> = mutableMapOf()
+    private val otherMonkeys: MutableMap<Int, Monkey> = mutableMapOf(),
+    var inspections: UInt = 0U
 ) {
     companion object {
-        fun from(input: String): Monkey {
+        fun from(input: String, worryDecreases: Boolean): Monkey {
             val lines = input.split("\n").map { it.trim() }
-            val itemWorryScores = Regex("(\\d+)+").findAll(lines[1]).map { it.value.toInt() }.toList()
-            val testCondition = Regex("(\\d+)+").find(lines[3])!!.value.toInt()
+            val itemWorryScores = Regex("(\\d+)+").findAll(lines[1]).map { it.value.toLong() }.toList()
+            val testCondition = Regex("(\\d+)+").find(lines[3])!!.value.toLong()
             val monkeyThrowOnTrue = Regex("(\\d+)+").find(lines[4])!!.value.toInt()
             val monkeyThrowOnFalse = Regex("(\\d+)+").find(lines[5])!!.value.toInt()
 
             return Monkey(
                 itemWorryScores.map { Item(it) }.toMutableList(),
-                parseUpdateFunction(lines[2]),
-                { worry -> worry % testCondition == 0 },
+                parseUpdateFunction(lines[2], worryDecreases, testCondition),
                 monkeyThrowOnTrue,
                 monkeyThrowOnFalse
             )
         }
 
-        private fun parseUpdateFunction(input: String) =
+        private fun parseUpdateFunction(input: String, worryDecreases: Boolean, testCondition: Long) =
             Regex("Operation: new = (old|\\d+) ([+\\-*/]) (old|\\d+)").find(input)!!.groupValues
                 .let { (_, firstOperand, operator, secondOperand) ->
-                    { worry: Int ->
+                    { worry: Long ->
                         val parsedFirstOperand = when (firstOperand) {
-                            "old" -> worry.toDouble()
-                            else -> firstOperand.toDouble()
+                            "old" -> worry
+                            else -> firstOperand.toLong()
                         }
 
                         val parsedSecondOperand = when (secondOperand) {
-                            "old" -> worry.toDouble()
-                            else -> secondOperand.toDouble()
+                            "old" -> worry
+                            else -> secondOperand.toLong()
                         }
 
-                        val parsedOperator: (Double, Double) -> Double = when (operator) {
-                            "+" -> Double::plus
-                            "-" -> Double::minus
-                            "*" -> Double::times
-                            "/" -> Double::div
+                        val parsedOperator: (Long, Long) -> Long = when (operator) {
+                            "+" -> Long::plus
+                            "-" -> Long::minus
+                            "*" -> Long::times
                             else -> throw RuntimeException("Unknown operator: $operator")
                         }
 
-                        floor(
-                            parsedOperator(
-                                parsedFirstOperand,
-                                parsedSecondOperand
-                            ) / 3.0
-                        ).toInt()
+                        if (worryDecreases) {
+                            parsedOperator(parsedFirstOperand, parsedSecondOperand).floorDiv(3L)
+                        } else {
+                            parsedOperator(parsedFirstOperand, parsedSecondOperand)
+                        }.mod(testCondition)
                     }
                 }
     }
@@ -86,26 +104,26 @@ class Monkey private constructor(
         items.forEach {
             inspections++
             it.update(inspectFun)
-            if (checkCondition(it.worryScore)) passItem(it, throwOnTrue) else passItem(it, throwOnFalse)
+            passItem(it)
         }
         items.clear()
-    }
-
-    fun catchItem(item: Item) {
-        items.add(item)
     }
 
     fun monkeyDiscovered(identity: Int, monkey: Monkey) {
         otherMonkeys[identity] = monkey
     }
 
-    private fun passItem(item: Item, identity: Int) {
-        otherMonkeys[identity]!!.catchItem(item)
+    private fun catchItem(item: Item) {
+        items.add(item)
+    }
+
+    private fun passItem(item: Item) {
+        otherMonkeys[if (item.worryScore == 0L) throwOnTrue else throwOnFalse]!!.catchItem(item)
     }
 }
 
-class Item(var worryScore: Int) {
-    fun update(updateFn: (Int) -> Int) {
+class Item(var worryScore: Long) {
+    fun update(updateFn: (Long) -> Long) {
         worryScore = updateFn(worryScore)
     }
 }
--- src/test/kotlin/Day11Test.kt
+++ src/test/kotlin/Day11Test.kt
@@ -2,25 +2,7 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class Day11Test {
-    // Items (worry score)
-    // Operation:
-    //  - old or number
-    //  - operator
-    // Test:
-    //  - Divisible by 'x'
-    // Throw:
-    // - true to monkey 'y'
-    // - false to monkey 'y'
-    //
-    // 1: monkey
-    // 2: items
-    // 3: operation
-    // 4: test condition
-    // 5: true throw
-    // 6: false throw
-    @Test
-    fun solveDay11A() {
-        val input = """
+    val input = """
         Monkey 0:
           Starting items: 79, 98
           Operation: new = old * 19
@@ -50,8 +32,33 @@ class Day11Test {
             If false: throw to monkey 1
         """.trimIndent().lines()
 
+    // Items (worry score)
+    // Operation:
+    //  - old or number
+    //  - operator
+    // Test:
+    //  - Divisible by 'x'
+    // Throw:
+    // - true to monkey 'y'
+    // - false to monkey 'y'
+    //
+    // 1: monkey
+    // 2: items
+    // 3: operation
+    // 4: test condition
+    // 5: true throw
+    // 6: false throw
+    @Test
+    fun solveDay11A() {
         val actual = Day11.solveA(input)
+        assertEquals(10605U, actual)
+    }
 
-        assertEquals(10605, actual)
+    // Numbers tooooo big...
+    /// Maths; is truncating to "hundreds" units viable after each test
+    @Test
+    fun solvedDay11B() {
+        val actual = Day11.solveB(input)
+        assertEquals(2713310158U, actual)
     }
 }
